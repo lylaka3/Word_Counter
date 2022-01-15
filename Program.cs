c00698c1ca273d276d2463ec45b87e21f6a67c7c
lylaka3 <56922691+lylaka3@users.noreply.github.com>
2022-01-15 23:13:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Diagnostics;
+
+namespace Тест_Digital_Design
+{    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            string path = @"ViM1.txt";
+            string result = @"result.txt";
+            using (StreamReader sr = new StreamReader(path))
+            {
+                text = sr.ReadToEnd();
+            }
+            using (StreamWriter sw = new StreamWriter(result, false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine("Итого:\n");
+            }
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            Assembly assembly = Assembly.Load(File.ReadAllBytes("Transfer.dll"));
+            Type type = assembly.GetType("Transfer.TransferClass");
+            MethodInfo method = type.GetMethod("MakeDictionaryParallel", BindingFlags.Instance | BindingFlags.NonPublic);
+            object instance = Activator.CreateInstance(type);
+            Dictionary<string, int> wordlist = (Dictionary<string, int>)method.Invoke(instance, new object[] { text });
+
+            stopWatch.Stop();
+            Console.WriteLine("Время записи в словарь: " + stopWatch.Elapsed);
+
+            Console.WriteLine("Идёт запись...");
+            foreach (var pair in wordlist.OrderByDescending(pair => pair.Value))
+            {
+                using (StreamWriter sw = new StreamWriter(result, true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine("{0} - {1}", pair.Key, pair.Value);
+                }
+            }            
+        }
+    }
+}
---
+++ TransferClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using System.Collections.Concurrent;
+
+namespace Transfer
+{
+    class TransferClass
+    {
+        private Dictionary<string, int> MakeDictionary(string text)
+        {
+            Dictionary<string, int> wordlist = new Dictionary<string, int>();
+            text = text.ToLower();
+            string[] words = text.Split(new char[] { ' ', ',', '-', '.', '!', '?', '(', ')', '[', ']', '\"', '\'', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string word in words)
+            {
+                if (wordlist.ContainsKey(word))
+                    wordlist[word]++;
+                else
+                    wordlist.Add(word, 1);
+            }
+            return wordlist;
+        }
+        private Dictionary<string, int> MakeDictionaryParallel(string text)
+        {
+            ConcurrentDictionary<string, int> wordlist = new ConcurrentDictionary<string, int>();
+            text = text.ToLower();
+            string[] words = text.Split(new char[] { ' ', ',', '-', '.', '!', '?', '(', ')', '[', ']', '\"', '\'', '\n', '\r', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+            
+            
+            Parallel.ForEach<string>(words, word => {
+                if (wordlist.ContainsKey(word))
+                    wordlist[word]++;
+                else
+                    wordlist.TryAdd(word, 1);
+            });            
+            return wordlist.ToDictionary(k => k.Key, v => v.Value);
+        }
+    }
+}
